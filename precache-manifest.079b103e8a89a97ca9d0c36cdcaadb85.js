c480fc5ab2d5f8468c779cec3c07671982d5f548
Jessie Wu <jessiewu@Jessies-Air.hitronhub.home>
2020-07-10 20:55:30 -0400

    Updates

---
+++ precache-manifest.079b103e8a89a97ca9d0c36cdcaadb85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbaa38685337408765e6f5786f32692",
+    "url": "/github.io/weather-app-final/index.html"
+  },
+  {
+    "revision": "eacdd50458ede91b6306",
+    "url": "/github.io/weather-app-final/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "0ab5b615f61c731fe84e",
+    "url": "/github.io/weather-app-final/static/css/main.12fce6fe.chunk.css"
+  },
+  {
+    "revision": "eacdd50458ede91b6306",
+    "url": "/github.io/weather-app-final/static/js/2.bc0d7cfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/weather-app-final/static/js/2.bc0d7cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab5b615f61c731fe84e",
+    "url": "/github.io/weather-app-final/static/js/main.d4358f06.chunk.js"
+  },
+  {
+    "revision": "e3e0ea23c89f7d73cda4",
+    "url": "/github.io/weather-app-final/static/js/runtime-main.605dc204.js"
+  },
+  {
+    "revision": "7a1a1dd50a3a01e2d0c46cadf9eb3830",
+    "url": "/github.io/weather-app-final/static/media/bg.7a1a1dd5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/weather-app/precache-manifest.88a49a19df76491d3fe0b69bf5898453.js"
+  "/github.io/weather-app-final/precache-manifest.079b103e8a89a97ca9d0c36cdcaadb85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/weather-app-final/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
